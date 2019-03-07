acd63f2cdcdff58bcdc4c6d52c72facc23c164eb
oksanterova <oksana.kanterova@gmail.com>
2019-03-07 19:52:07 +0100

    Configure react-redux and redux-promise

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// calling the connect wrapper and the function created in the actions section
+import { connect } from "react-redux";
+import { defaultFunction } from "./actions";
 
 class App extends Component {
+  componentDidMount() {
+    // call default function to display redux operation
+    this.props.defaultFunction();
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div>React Redux Starter Template</div>;
   }
 }
 
-export default App;
+// function to convert the global state obtained from redux to local props
+function mapStateToProps(state) {
+  return {
+    default: state.default
+  };
+}
+
+// wrapping the component within the connect HOC and calling the default function directly
+export default connect(
+  mapStateToProps,
+  { defaultFunction }
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import reducers from "./reducers";
+import ReduxPromise from "redux-promise";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// creating a store for the redux structure
+const store = createStore(reducers, applyMiddleware(logger, ReduxPromise));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
